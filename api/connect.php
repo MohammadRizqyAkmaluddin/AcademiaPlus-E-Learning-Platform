63102b5f4505bc9d33ae3375084760679aa9e0a3
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2025-09-24 04:59:49 +0700

    Latest Update

--- api/connect.php
+++ api/connect.php
@@ -1,12 +1,8 @@
-<?php
-$host = getenv("DB_HOST") ?: "localhost";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "";
-$db   = getenv("DB_NAME") ?: "academiaplus";
-
+<?php $host = "localhost";
+$user = "root";
+$pass = "";
+$db = "academiaplus";
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
-    echo "Failed to connect DB: " . $conn->connect_error;
+    echo "Failed to connect DB" . $conn->connect_error;
 }
-?>
